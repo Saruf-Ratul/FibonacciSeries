f7fbb1afad60b6388e00b2ca421feb227bea1ce7
Saruf <saruftoratul95@gmail.com>
2021-05-11 11:47:17 +0600

    confirm

---
+++ src/fibonacciseries/FibonacciSeries.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fibonacciseries;
+
+/**
+ *
+ * @author Saruf
+ */
+public class FibonacciSeries {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+           int n = 100, firstTerm = 0, secondTerm = 1;
+        
+    System.out.println("Fibonacci Series Upto " + n + ": ");
+    
+    while (firstTerm <= n) {
+      System.out.print(firstTerm + ", ");
+
+      int nextTerm = firstTerm + secondTerm;
+      firstTerm = secondTerm;
+      secondTerm = nextTerm;
+    }
+    }
+    
+}
